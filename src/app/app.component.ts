f4a64e267ab55db7ae34777de30b137541844aa8
DawoodSaeed <dawoodsaeed18@gmail.com>
2024-08-31 02:56:42 +0500

    Working on the login screen. Initial Setup is done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '../button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  onSubmit(arg0: any) {
+    throw new Error('Method not implemented.');
+  }
   title = 'catering-system';
 }
