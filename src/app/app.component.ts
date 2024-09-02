8ed95902aefb930d96f51c10210671e15db74be8
DawoodSaeed <dawoodsaeed18@gmail.com>
2024-09-02 08:15:03 +0500

    layout changes in register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '../button/button.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, FormsModule],
+  imports: [RouterOutlet, ButtonComponent, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
